9424ed830039789f8671829f014640734d48cf83
Luiz Fernando <luizepauloxd@gmail.com>
2020-11-27 22:10:08 -0300

    feat: adds lottie animations

---
+++ src/animations/index.ts
@@ -0,0 +1,4 @@
+export { default as Articles } from "./articles.json"
+export { default as ContactUs } from "./contact-us.json"
+export { default as Projects } from "./projects.json"
+export { default as Error404 } from "./404.json"
